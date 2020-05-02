69fa44a2f83787b2922ad7f0aedbb854587f9cb1
Caio Favoretto <caiowfavoretto@gmail.com>
2020-05-02 20:41:22 -0300

    feature: like and list repositories

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: 'https://67dd1b86.ngrok.io',
 });
 
 export default api;
